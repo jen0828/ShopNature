a8f886efbbd4ba19b48902ead31c5c62a4569eea
Jenny Moon <jenny0moon@gmail.com>
2022-01-11 19:55:07 +0000

    Remove redundant space

--- src/App.js
+++ src/App.js
@@ -2,39 +2,39 @@ import product from './product.jpg';
 import product1 from './product-1.jpg';
 import product2 from './product-2.jpg';
 import './App.css';
-import { Navbar, Container, Nav, NavDropdown, Button} from 'react-bootstrap';
+import { Navbar, Container, Nav, NavDropdown, Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <Navbar bg="light" expand="lg">
-  <Container>
-    <Navbar.Brand href="#home">Organic Cosmetics</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="me-auto">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#link">Link</Nav.Link>
-        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-        </NavDropdown>
-      </Nav>
-    </Navbar.Collapse>
-  </Container>
-</Navbar>
+        <Container>
+          <Navbar.Brand href="#home">Organic Cosmetics</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Link</Nav.Link>
+              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
 
 
-<div className="jumbotron">
-  <h1>SPRING COLLECTION</h1>
-  
-  <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-  <Button variant="secondary">Learn More</Button>{' '} 
-</div>
-<br></br>
+      <div className="jumbotron">
+        <h1>SPRING COLLECTION</h1>
+
+        <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+        <Button variant="secondary">Learn More</Button>{' '}
+      </div>
+      <br></br>
       <div className="container">
         <div className="row">
           <div className="col-md-4">
@@ -43,12 +43,12 @@ function App() {
             <p>Detail</p>
           </div>
           <div className="col-md-4">
-            <img src={product1} width="100%" alt=''/>
+            <img src={product1} width="100%" alt='' />
             <h4>Product</h4>
             <p>Detail</p>
           </div>
           <div className="col-md-4">
-            <img src={product2} width="100%" alt=''/>
+            <img src={product2} width="100%" alt='' />
             <h4>Product</h4>
             <p>Detail</p>
           </div>
