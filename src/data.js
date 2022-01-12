47081764212e7725736a6b93b6a1f40943c50060
Jenny Moon <jenny0moon@gmail.com>
2022-01-12 09:57:57 +0000

    Add seed data

---
+++ src/data.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    id : 0,
+    title : "DAILY RENEW FACIAL CREAM",
+    content : "Pure organic",
+    price : 60
+  },
+
+  {
+    id : 1,
+    title : "MIRACLE FACIAL OIL",
+    content : "Pure organic",
+    price : 70
+  },
+
+  {
+    id : 2,
+    title : "HYDRONIC SERUM 200",
+    content : "Pure organic",
+    price : 60
+  }
+]
