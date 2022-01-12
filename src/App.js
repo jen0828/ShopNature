bc3762c1b49d8d8296bf9cb4a824861774ac044f
Jenny Moon <jenny0moon@gmail.com>
2022-01-12 16:17:50 +0000

    Set up multi-pages

--- src/App.js
+++ src/App.js
@@ -16,18 +16,30 @@ function App() {
           < NavBar />
           <Jumbotron />
           <br></br>
-            <div className="container">
-              <div className="row">
-                {
-                  products.map((a, i) => {
-                    return <Card products={products[i]} i={i} key={i} />
-                  })};
-              </div>
+          <div className="container">
+            <div className="row">
+              {
+                products.map((a, i) => {
+                  return <Card products={products[i]} i={i} key={i} />
+                })};
             </div>
+          </div>
         </Route>
         <Route exact path="/detail">
-          <div>Detail Page!</div>
-        </Route> 
+          <div className="container">
+            <div className="row">
+              <div className="col-md-6">
+                <img src="https://images.unsplash.com/photo-1602409339188-95d178a611a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80" width="100%" />
+              </div>
+              <div className="col-md-6 mt-4">
+                <h4 className="pt-5">Product</h4>
+                <p>Details</p>
+                <p>£60.00</p>
+                <button className="btn btn-danger">Order</button>
+              </div>
+            </div>
+          </div>
+        </Route>
       </Router>
     </div>
 
