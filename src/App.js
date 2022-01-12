9fd7ebc96b2e0eb3d42e885ccf83da1d3dc88fbf
Jenny Moon <jenny0moon@gmail.com>
2022-01-12 16:24:57 +0000

    Create detail-page component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import DetailPage from './DetailPage'
 import NavBar from './NavBar'
 import Jumbotron from './Jumbotron'
 import './App.css';
@@ -26,19 +27,7 @@ function App() {
           </div>
         </Route>
         <Route exact path="/detail">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-6">
-                <img src="https://images.unsplash.com/photo-1602409339188-95d178a611a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80" width="100%" />
-              </div>
-              <div className="col-md-6 mt-4">
-                <h4 className="pt-5">Product</h4>
-                <p>Details</p>
-                <p>£60.00</p>
-                <button className="btn btn-danger">Order</button>
-              </div>
-            </div>
-          </div>
+          <DetailPage />
         </Route>
       </Router>
     </div>
