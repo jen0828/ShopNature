edd2690962f8ce994a34b90fe398c05916ca484d
Jenny Moon <jenny0moon@gmail.com>
2022-01-12 23:43:37 +0000

    Add alert message function

--- src/ProductDetails.js
+++ src/ProductDetails.js
@@ -1,12 +1,19 @@
 import { useHistory, useParams } from 'react-router-dom';
-
+import React, { useState, useEffect } from 'react'
 
 export default function ProductDetails(props) {
   let { id } = useParams();
   let history = useHistory();
-  let myProduct = props.products.find(function(product){
+  let myProduct = props.products.find(function (product) {
     return product.id == id
   });
+
+  let [alert, setAlert] = useState(true);
+  useEffect(() => {
+    let timer = setTimeout(() => { setAlert(false) }, 3000);
+
+    return () => { clearTimeout(timer) }
+  }, []);
 
   return (
     <div>
@@ -14,16 +21,25 @@ export default function ProductDetails(props) {
         <div className="row">
           <div className="col-md-6 mt-4">
             <div className="pt-5">
-            <img src={myProduct.image} width="100%" alt='' /> 
-            <h4>{myProduct.title}</h4>
-            <p>{myProduct.content}</p>
-            <p>{myProduct.price}</p>
-            <button className="btn btn-secondary">Order</button>
-            <button onClick={()=>{ history.goBack() }} className="btn btn-secondary">Go back</button>
-            </div> 
+              <img src={myProduct.image} width="100%" alt='' />
+              <h4>{myProduct.title}</h4>
+              <p>{myProduct.content}</p>
+              <p>{myProduct.price}</p>
+              <button className="btn btn-secondary">Order</button>
+              <button onClick={() => { history.goBack() }} className="btn btn-secondary">Go back</button>
+            </div>
           </div>
         </div>
       </div>
+
+      {
+        alert === true
+          ? (<div className="my-alert2">
+            <p>Only few in stock</p>
+          </div>)
+          : null
+      }
     </div>
+
   )
 }
