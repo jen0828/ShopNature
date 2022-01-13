634f19281967d6ab4139262c46a35932d16961ca
Jenny Moon <jenny0moon@gmail.com>
2022-01-13 13:11:59 +0000

    Add new functionality: moving to detail page when product card is clicked

--- src/ProductCard.js
+++ src/ProductCard.js
@@ -1,6 +1,9 @@
+import { useHistory } from 'react-router-dom';
+
 export default function ProductCard(props) {
+  let history = useHistory();
   return (
-    <div className="col-md-4">
+    <div className="col-md-4" onClick={() => { history.push('/detail/' + props.products.id) }}>
       <img src={props.products.image} width="100%" alt='' />
       <h4>{props.products.title}</h4>
       <p>{props.products.content} & {props.products.price}</p>
