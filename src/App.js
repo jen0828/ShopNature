de5368e63be0c7dd31afab7715b42e7edf2d008c
Jenny Moon <jenny0moon@gmail.com>
2022-01-13 11:18:41 +0000

    Add new functionality: fetch data from api when button is clicked

--- src/App.js
+++ src/App.js
@@ -33,12 +33,11 @@ function App() {
         </Route>
       </Router>
 
-
       <button className="btn btn-secondary" onClick={() => {
 
-        axios.get('http://localhost:8000/products')
-          .then((result) => { console.log(result.data) })
-          .catch(() => { "error!" })
+        axios.get('http://localhost:8000/product')
+          .then((result) => { setProducts([...products, ...result.data]) })
+          .catch(() => { console.log('Errors!')})
 
       }}>See more</button>
     </div>
