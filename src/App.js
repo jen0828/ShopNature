fde31a8611ee28739a959d92cf1563e9f37e70d7
Jenny Moon <jenny0moon@gmail.com>
2022-01-13 10:54:44 +0000

    Set up ajax using axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import ProductCard from './ProductCard'
-import ProductDetails from './ProductDetails'
-import NavBar from './NavBar'
-import Jumbotron from './Jumbotron'
+import ProductCard from './ProductCard';
+import ProductDetails from './ProductDetails';
+import NavBar from './NavBar';
+import Jumbotron from './Jumbotron';
 import './App.css';
-import React, { useState } from 'react'
-import Data from './data.js'
+import React, { useState } from 'react';
+import Data from './data.js';
+import axios from 'axios';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
@@ -31,6 +32,15 @@ function App() {
           <ProductDetails products={products} />
         </Route>
       </Router>
+
+
+      <button className="btn btn-secondary" onClick={() => {
+
+        axios.get('http://localhost:8000/products')
+          .then((result) => { console.log(result.data) })
+          .catch(() => { "error!" })
+
+      }}>See more</button>
     </div>
 
   )
