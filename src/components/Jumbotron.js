890ce7b23e5d317e338a46c7b581d3ecee2e1de5
Jenny Moon <jenny0moon@gmail.com>
2022-01-16 19:31:40 +0000

    Add new page link to read more button

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,14 +1,16 @@
 import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import ProductCard from './ProductCard';
 
 export default function Jumbotron() {
   return (
-    
-      <div className="jumbotron">
-        <h1>SPRING COLLECTION</h1>
-        <p>Find your new natural routine with our vegan face care range. It's time to Evolve your face skincare routine.</p>
-        <Button variant="secondary">Learn More</Button>{' '}
-      </div>
-    
+
+    <div className="jumbotron">
+      <h1>SPRING COLLECTION</h1>
+      <p>Find your new natural routine with our vegan face care range. It's time to Evolve your face skincare routine.</p>
+      <Button variant="secondary" as={Link} to="/detail">Learn More</Button>{' '}
+    </div>
+
   )
 }
 
