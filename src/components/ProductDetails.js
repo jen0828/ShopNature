c84e974e09124128861d92e3e736e2abed2bad08
Jenny Moon <jenny0moon@gmail.com>
2022-01-16 15:27:09 +0000

    Update styling

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,6 +1,7 @@
 import { useHistory, useParams } from 'react-router-dom';
 import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux';
+import './ProductDetails.css';
 
 function ProductDetails(props) {
   let { id } = useParams();
@@ -19,33 +20,33 @@ function ProductDetails(props) {
   return (
     <div>
       <div className="container">
-        <div className="row">
+        {
+          alert === true
+            ? (<div className="my-alert">
+              <p>Only few in stock</p>
+            </div>)
+            : null
+        }
+        <div className="row justify-content-md-center">
           <div className="col-md-6 mt-4">
             <div className="pt-5">
               <img src={myProduct.image} width="100%" alt='' />
               <h4>{myProduct.title}</h4>
               <p>{myProduct.content}</p>
               <p>{myProduct.price}</p>
-              <button className="btn btn-secondary">Order</button>
-              <button onClick={() => { history.goBack() }} className="btn btn-secondary">Go back</button>
-              <button onClick={() => {
+
+              <button className="btn btn-secondary" onClick={() => {
                 props.dispatch({ type: 'Add to Basket', data: { id: myProduct.id, name: myProduct.title, qty: 1 } })
                 history.push('/cart');
 
-              }}>Add to Basket</button>
+              }}>Add to Cart</button>{' '}
 
+              <button onClick={() => { history.goBack() }} className="btn btn-secondary">Go back</button>
             </div>
           </div>
         </div>
       </div>
 
-      {
-        alert === true
-          ? (<div className="my-alert2">
-            <p>Only few in stock</p>
-          </div>)
-          : null
-      }
     </div>
 
   )
--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ export default [
     id : 0,
     image: 'https://images.unsplash.com/photo-1602409339188-95d178a611a0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80',
     title : "DAILY RENEW FACIAL CREAM",
-    content : "Our natural hyaluronic acid serum helps to hydrate your skin from within and plump fine lines and wrinkles. ",
+    content : "Pure organic",
     price : '£60.00'
   },
 
@@ -11,7 +11,7 @@ export default [
     id : 1,
     image: 'https://images.unsplash.com/photo-1564789225215-6343a80eafa3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80',
     title : "MIRACLE FACIAL OIL",
-    content : "Our natural hyaluronic acid serum helps to hydrate your skin from within and plump fine lines and wrinkles.",
+    content : "Pure organic",
     price : '£80.00'
   },
 
@@ -19,7 +19,7 @@ export default [
     id : 2,
     image: 'https://images.unsplash.com/photo-1581514439794-f9777f7c22eb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80',
     title : "HYDRONIC SERUM 200",
-    content : "Our natural hyaluronic acid serum helps to hydrate your skin from within and plump fine lines and wrinkles.",
+    content : "Pure organic",
     price : '£50.00'
   }
 ]
