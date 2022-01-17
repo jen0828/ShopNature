f7b42c603ce00d0fcc6081be567ce0b36128e850
Jenny Moon <jenny0moon@gmail.com>
2022-01-17 09:55:33 +0000

    Add hover effect to product card

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -4,8 +4,8 @@ export default function ProductCard(props) {
   let history = useHistory();
   return (
     <div className="col-md-4" onClick={() => { history.push('/detail/' + props.products.id) }}>
-      <img src={props.products.image} width="100%" alt='' />
-      <h4>{props.products.title}</h4>
+      <img className='product-card' src={props.products.image} width="100%" alt='product-card'/>
+      <h4 >{props.products.title}</h4>
       <p>{props.products.content}</p>
       <p>{props.products.price}</p>
     </div>
