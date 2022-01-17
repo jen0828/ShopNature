3158a2138886e904a78aee17109071e717e2db3b
Jenny Moon <jenny0moon@gmail.com>
2022-01-17 14:45:03 +0000

    Add more images

--- src/data.js
+++ src/data.js
@@ -21,5 +21,27 @@ export default [
     title : "HYDRONIC SERUM 200",
     content : "Pure organic",
     price : '£50.00'
-  }
+  },
+
+  {
+    id:3,
+    image:"https://images.unsplash.com/photo-1624454002302-36b824d7bd0a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    title:"DAILY RENEW FACIAL OIL",
+    content:"Pure organic",
+    price:"£60.00"
+ },
+ {
+    id:4,
+    image:"https://images.unsplash.com/photo-1614159102522-81c48758428f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    title:"ROSE CLAY",
+    content:"Pure organic",
+    price:"£70.00"
+ },
+ {
+    id:5,
+    image:"https://images.unsplash.com/photo-1596470663178-dc2df28026f7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+    title:"BLOSSOM OIL",
+    content:"Pure organic",
+    price:"£60.00"
+ }
 ]
