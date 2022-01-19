ea282f9be545dd750d202fd841f9ae85cee68262
Jenny Moon <jenny0moon@gmail.com>
2022-01-19 12:26:38 +0000

    Update styling

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -5,7 +5,7 @@ export default function ProductCard(props) {
   return (
     <div className="col-md-4" onClick={() => { history.push('/detail/' + props.products.id) }}>
       <img className='product-card' src={props.products.image} width="100%" alt='product-card'/>
-      <h4 >{props.products.title}</h4>
+      <h4 className='title'>{props.products.title}</h4>
       <p>{props.products.content}</p>
       <p>{props.products.price}</p>
     </div>
