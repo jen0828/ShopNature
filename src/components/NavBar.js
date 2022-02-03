cb6656e09f54e1eac46f3bfafa3cb3a84f2ba90e
Jenny Moon <jenny0moon@gmail.com>
2022-02-03 20:10:48 +0000

    Update navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,4 +1,4 @@
-import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
+import { Navbar, Container, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 function NavBar() {
@@ -12,16 +12,14 @@ function NavBar() {
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
               <Nav.Link as={Link} to="/">Home</Nav.Link>
-              <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
-              <Nav.Link as={Link} to="/cart">Cart</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
+              <Nav.Link as={Link} to="/learnmore">About</Nav.Link>
+              <Nav.Link as={Link} to="/contact">Contact</Nav.Link>  
             </Nav>
+            <Nav className="d-flex">
+            <Nav.Link as={Link} to="/">Login</Nav.Link>
+            <Nav.Link as={Link} to="/">Signup</Nav.Link>
+            <Nav.Link as={Link} to="/cart">Cart</Nav.Link>
+            </Nav> 
           </Navbar.Collapse>
         </Container>
       </Navbar>
