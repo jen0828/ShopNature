b98fee36810403edd41f7441315c77cb9bb7887c
Jenny Moon <jenny0moon@gmail.com>
2022-02-04 18:07:44 +0000

    Edit reducer initialstate

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,5 +1,5 @@
 
-let initialState = [{ id: 0, name: 'DAILY RENEW FACIAL CREAM', qty: 1 }];
+let initialState = [{ id: 0, name: 'DAILY RENEW FACIAL CREAM', qty: 0 }];
 
 
 export default function reducer(state = initialState, action) {
