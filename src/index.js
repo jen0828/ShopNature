c2e87aec76159a4e677f62a055cd64fe624dd097
Jenny Moon <jenny0moon@gmail.com>
2022-02-04 18:04:47 +0000

    Update import section

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle'
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import '../node_modules/font-awesome/css/font-awesome.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import '../node_modules/font-awesome/css/font-awesome.min.css';
 import { Provider } from 'react-redux';
-import store from './redux/store'
+import store from './redux/store';
 
 ReactDOM.render(
   <React.StrictMode>
