d1150cdd2b881559b9567b3fb9e18488f4168cb9
Jenny Moon <jenny0moon@gmail.com>
2022-02-05 15:41:32 +0000

    Fix bug

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -25,16 +25,16 @@ const Signup = () => {
                           <form>
                               <div className="mb-3">
                                   <label htmlFor="exampleInput" className="form-label">Username</label>
-                                  <input type="text" className="form-control" id="exampleInput" />
+                                  <input type="text" className="form-control" id="exampleInput" autoComplete="username"/>
                               </div>
                               <div className="mb-3">
-                                  <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-                                  <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                  <label htmlFor="exampleInputEmail2" className="form-label">Email address</label>
+                                  <input type="email" className="form-control" id="exampleInputEmail2" aria-describedby="emailHelp" />
                                   <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                               </div>
                               <div className="mb-3">
-                                  <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-                                  <input type="password" className="form-control" id="exampleInputPassword1" />
+                                  <label htmlFor="exampleInputPassword2" className="form-label">Password</label>
+                                  <input type="password" className="form-control" id="exampleInputPassword2" autoComplete="current-password" />
                               </div>
                               <div className="mb-3 form-check">
                                   <input type="checkbox" className="form-check-input" id="exampleCheck1" />
