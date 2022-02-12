921724d6ca7c566dd66db0233d2c7999bcd36c5e
Jenny Moon <jenny0moon@gmail.com>
2022-02-12 12:53:16 +0000

    Update e2e test

--- cypress/integration/myapp.spec.js
+++ cypress/integration/myapp.spec.js
@@ -1,34 +1,32 @@
 describe('my app', () => {
 
-  it('can select multiple products and add to the cart', () => {
+  it('can select/add products to the cart and then checkout', () => {
       cy.visit('http://localhost:3000')
       
       //Add first item
-      cy.findByRole('heading', {  name: /miracle facial oil/i}).click()
-      
-      cy.findByRole('button', {  name: /add to cart/i}).click()
+      cy.findByRole('heading', {  name: /miracle facial oil/i}).click();
+      cy.findByRole('button', {  name: /add to cart/i}).click();
+      cy.findByRole('button', {  name: /added to cart!/i});
+      cy.findByRole('link', {  name: /1/i});
+      cy.findByRole('button', {  name: /go back/i}).click();
 
-      cy.findByRole('cell', {  name: /miracle facial oil/i})
+      // Add second item
+      cy.findByRole('heading', {  name: /bio\-retinol \+ c skin booster/i}).click();
+      cy.findByRole('button', {  name: /add to cart/i}).click();
+      cy.findByRole('button', {  name: /added to cart!/i});
+      cy.findByRole('link', {  name: /2/i});
+      cy.findByRole('button', {  name: /go back/i}).click();
 
-      cy.findByRole('link', {  name: /home/i}).click()
+      // Add third item
+      cy.findByRole('heading', {  name: /blossom oil/i}).click();
+      cy.findByRole('button', {  name: /add to cart/i}).click();
+      cy.findByRole('button', {  name: /added to cart!/i});
+      cy.findByRole('link', {  name: /3/i});
 
-      //Add second item
-
-      cy.findByRole('heading', {  name: /hydronic serum 200/i}).click()
-
-      cy.findByRole('button', {  name: /add to cart/i}).click()
-
-      cy.findByRole('cell', {  name: /hydronic serum 200/i})
-
-      cy.findByRole('link', {  name: /home/i}).click()
-
-      //Add third item
-
-      cy.findByRole('heading', {  name: /bio\-retinol \+ c skin booster/i}).click()
-
-      cy.findByRole('button', {  name: /add to cart/i}).click()
-
-      cy.findByRole('cell', {  name: /bio\-retinol \+ c skin booster/i})
-
+      // Checkout
+      cy.findByRole('link', {  name: /3/i}).click();
+      cy.findByRole('link', {  name: /checkout/i}).click();
+      cy.findByRole('heading', {  name: /your cart 3/i});
+      cy.findByText(/total \(gbp\)/i).should("be.visible");
   })
 })
