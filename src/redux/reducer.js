fb199c2b4310047abbc20bbd9a5f7e345da57732
Jenny Moon <jenny0moon@gmail.com>
2022-02-15 10:30:30 +0000

    Refactor: using switch statement

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -5,43 +5,39 @@ export default function reducer(state = initialState, action) {
   
   let sameItemFinder = state.findIndex((a) => { return a.id === action.data.id });
   
-  if (action.type === 'Add to Basket') {
-
-        if (sameItemFinder >= 0) {
-          const copy= [...state];
+  switch(action.type){
+      case 'Add to Basket':
+          if (sameItemFinder >= 0) {
+          let copy= [...state];
           copy[sameItemFinder].qty++
           copy[sameItemFinder].price += action.data.price
           return copy
-        } else {
+         } else {
           let copyItem = [...state];
           copyItem.push(action.data)
           return copyItem
-        }
+         } 
 
-  } else if (action.type === 'Delete item') { 
-    return state = state.filter((x)=>{
-      return x.id !== action.data.id
-  })
+    case 'Delete item':
+        return state = state.filter((x)=>{
+          return x.id !== action.data.id
+        })
 
-  } else if (action.type === 'Qty increase') {
+    case 'Qty increase':
+        let copy = [...state];
+        copy[sameItemFinder].qty++;
+        let oneItemPrice = (action.data.price)/(action.data.qty);
+        copy[sameItemFinder].price += oneItemPrice;
+        return copy
 
-    let copy = [...state];
-    copy[sameItemFinder].qty++;
-    let oneItemPrice = (action.data.price)/(action.data.qty);
-    copy[sameItemFinder].price += oneItemPrice;
-    return copy
+    case 'Qty decrease':
+        let copy2 = [...state];
+        copy2[sameItemFinder].qty--;
+        let oneItemPrice2 = (action.data.price)/(action.data.qty);
+        copy2[sameItemFinder].price -= oneItemPrice2;
+        return copy2
 
-  } else if (action.type === 'Qty decrease') {
-
-    let copy = [...state];
-    copy[sameItemFinder].qty--;
-    let oneItemPrice = (action.data.price)/(action.data.qty);
-    copy[sameItemFinder].price -= oneItemPrice;
-    return copy
-
-  } else {
-    return state
+    default :
+        return state
+   }
   }
-}
-
-
